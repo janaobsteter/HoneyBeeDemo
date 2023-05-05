da8985029089cce5af4bffc0e90252acb9fdab62
janaobsteter <obsteter.jana@gmail.com>
2023-05-05 13:21:19 +0100

    Working on a small dataset on my laptop

--- Snakemake/workflow/scripts/ExtractPosVcf.sh
+++ Snakemake/workflow/scripts/ExtractPosVcf.sh
@@ -3,8 +3,4 @@ alignedPos=$2
 output=$3
 outputPos=$4
 
-vcftools 	--gzvcf $vcf
-	        --out $3
-	        --positions $alignedPos
-	        --get-INFO AC
-	        --get-INFO AF
+vcftools --gzvcf $vcf --out $3 --positions $alignedPos --get-INFO AC  --get-INFO AF
